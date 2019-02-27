69434f8f29b387539d66ecb08fe344e526175aed
Ewan88 <baileyewan@gmail.com>
2019-02-27 10:47:40 +0000

    adds Vue3

---
+++ README.md
@@ -0,0 +1,44 @@
+
+## A Vue.js and MongoDB web application
+
+### Server
+
+Install server dependencies:
+
+```
+cd server
+npm install
+```
+
+Run a mongoDB server:
+
+```
+mongod
+```
+
+Seed the database:
+
+```
+npm run seeds
+```
+
+Run express:
+
+```
+npm run server:dev
+```
+
+### Client
+
+Project setup
+
+```
+cd ../client
+npm install
+```
+
+Compiles and hot-reloads for development
+
+```
+npm run serve
+```
---
+++ client/src/App.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <div id="app">
+    <h1>Hilary Clinton Hotels</h1>
+  </div>
+</template>
+
+<script>
+import { eventBus } from './main';
+export default {
+  name: 'app',
+  data () {
+    return {
+      bookings: []
+    }
+  },
+  components: {
+
+  },
+  mounted(){
+    this.fetchData();
+
+    eventBus.$on('refresh-data', this.fetchData);
+  },
+  methods: {
+    fetchData(){
+      fetch("localhost:3000/api/bookings")
+      .then(res => res.json())
+      .then(bookings => this.bookings = bookings);
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
